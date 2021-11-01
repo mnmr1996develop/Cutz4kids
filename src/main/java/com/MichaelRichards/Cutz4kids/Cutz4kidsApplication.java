6a6143ffd0fe2aac139610016e3565e142ce7573
mnmr1996develop <mnmr1996develop@gmail.com>
2021-11-01 17:42:25 -0400

    initalized project from start.spring.io with dependicies i think i need

---
+++ src/main/java/com/MichaelRichards/Cutz4kids/Cutz4kidsApplication.java
@@ -0,0 +1,13 @@
+package com.MichaelRichards.Cutz4kids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cutz4kidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cutz4kidsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MichaelRichards/Cutz4kids/Cutz4kidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MichaelRichards.Cutz4kids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cutz4kidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
