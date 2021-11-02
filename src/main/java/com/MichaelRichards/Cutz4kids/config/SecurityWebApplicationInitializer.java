8e4936c202f5d1c90a77cb7d6bf24fb5bc825331
mnmr1996develop <mnmr1996develop@gmail.com>
2021-11-02 15:59:19 -0400

    getting the security config started

---
+++ src/main/java/com/MichaelRichards/Cutz4kids/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.MichaelRichards.Cutz4kids.Config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
